ebc30b19be88431ab2abe811d1d8732f27f1aa96
김재원 <wodnjs9058@naver.com>
2025-04-26 23:52:07 +0900

    HTML file display using a GET request

--- src/fronted/main.js
+++ src/fronted/main.js
@@ -1,16 +1,11 @@
-let regionData = [];
+//fetch("localhost:8000/data")
+//    .then((response) => response.json())
+//    .then((data) => console.log(data));
 
-fetch('http://localhost:8000')
-    .then(response => {
-    if(!response)
-        throw new Error('네트워크 응답이 올바르지 않습니다.');
-
-        return response.json();
-    })
-    .then(data => {
-        regionData = data;
-        console.log('서버에서 받은 데이터',regionData);
-    })
-    .catch(error => {
-        console.error('데이터를 불러오는 중 에러 발생: ',error);
-    })
+fetch("localhost:8000", {
+    method:"GET",
+    headers:{
+        "Content-Type" : "applicaiton/json",
+    }
+    .then((response) => response.json())
+    .then((data) => console.log(data));
--- src/server/Server.java
+++ src/server/Server.java
@@ -4,37 +4,40 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpServer;
 import src.api.WeatherApi;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.List;
 
 public class Server {
-    public void run() throws IOException{
+    public void run() throws IOException {
         InetSocketAddress address = new InetSocketAddress(8000);
-        HttpServer httpServer = HttpServer.create(address,0);
+        HttpServer httpServer = HttpServer.create(address, 0);
 
-        httpServer.createContext("/",(exchange -> {
+        httpServer.createContext("/", (exchange -> {
+            //http method,path print
             String clienRequestHttpMethod = exchange.getRequestMethod();
             String path = exchange.getRequestURI().getPath();
-            System.out.println("request method:"+clienRequestHttpMethod + " Path:" + path);
+            System.out.println("request mdethod:" + clienRequestHttpMethod + " Path:" + path);
 
+            //get region data from csv
             CoordinateToRegionMapper coordinateToRegionMapper = new CoordinateToRegionMapper();
             List<String> result = coordinateToRegionMapper.getRegionList();
 
+            //jackson
             ObjectMapper objectMapper = new ObjectMapper();
             String JSON = objectMapper.writeValueAsString(result);
 
-            exchange.getResponseHeaders().set("Content-Type","application/json");
-            exchange.sendResponseHeaders(200,JSON.getBytes().length);//보낼 데이터 길이를 넣어야함
-            try(OutputStream os = exchange.getResponseBody()){
-                os.write(JSON.getBytes());
+            //out html file to 8000port from here
+            File mainHTML = new File("src/fronted/main.html");
+            byte[] HTMLBytes = Files.readAllBytes(mainHTML.toPath());
+            exchange.sendResponseHeaders(200,HTMLBytes.length);
+            exchange.getResponseHeaders().set("Content-Type", "text/html; charset=utf-8");
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(HTMLBytes);
             }
-
         }));
 
         httpServer.start();
