e81755c4944a925368aeb678f20215ed7f6e22dd
김재원 <wodnjs9058@naver.com>
2025-04-27 23:13:20 +0900

    send html,css,js S to C

--- src/fronted/main.js
+++ src/fronted/main.js
@@ -1,11 +1,18 @@
-//fetch("localhost:8000/data")
-//    .then((response) => response.json())
-//    .then((data) => console.log(data));
+let test = "테스트입니다."
+window.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:8000/data")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("받은 지역명 데이터:", data);
+        })
+        .catch((error) => {
+            console.error("데이터를 가져오는 중 에러 발생:", error);
+        });
+});
 
-fetch("localhost:8000", {
-    method:"GET",
-    headers:{
-        "Content-Type" : "applicaiton/json",
-    }
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+
--- src/server/Server.java
+++ src/server/Server.java
@@ -2,12 +2,8 @@ package src.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpServer;
-import src.api.WeatherApi;
-
 import java.io.*;
-import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
-import java.net.URL;
 import java.nio.file.Files;
 import java.util.List;
 
@@ -16,19 +12,12 @@ public class Server {
         InetSocketAddress address = new InetSocketAddress(8000);
         HttpServer httpServer = HttpServer.create(address, 0);
 
+        //root html파일 get 요청시
         httpServer.createContext("/", (exchange -> {
             //http method,path print
             String clienRequestHttpMethod = exchange.getRequestMethod();
             String path = exchange.getRequestURI().getPath();
-            System.out.println("request mdethod:" + clienRequestHttpMethod + " Path:" + path);
-
-            //get region data from csv
-            CoordinateToRegionMapper coordinateToRegionMapper = new CoordinateToRegionMapper();
-            List<String> result = coordinateToRegionMapper.getRegionList();
-
-            //jackson
-            ObjectMapper objectMapper = new ObjectMapper();
-            String JSON = objectMapper.writeValueAsString(result);
+            System.out.println("request method:" + clienRequestHttpMethod + " Path:" + path);
 
             //out html file to 8000port from here
             File mainHTML = new File("src/fronted/main.html");
@@ -39,6 +28,61 @@ public class Server {
                 os.write(HTMLBytes);
             }
         }));
+
+        // /main.css 요청시
+        httpServer.createContext("/main.css", (exchange -> {
+            //http method,path print
+            String clienRequestHttpMethod = exchange.getRequestMethod();
+            String path = exchange.getRequestURI().getPath();
+            System.out.println("request method:" + clienRequestHttpMethod + " Path:" + path);
+
+            //out css file to 8000/main.css from here
+            File mainCSS = new File("src/fronted/main.css");
+            byte[] CSSBytes = Files.readAllBytes(mainCSS.toPath());
+            exchange.sendResponseHeaders(200, CSSBytes.length);
+            exchange.getResponseHeaders().set("Content-Type", "text/css; charset=utf-8");
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(CSSBytes);
+            }
+        }));
+
+        // /main.js 요청시
+        httpServer.createContext("/main.js", (exchange -> {
+            //http method,path print
+            String clienRequestHttpMethod = exchange.getRequestMethod();
+            String path = exchange.getRequestURI().getPath();
+            System.out.println("request method:" + clienRequestHttpMethod + " Path:" + path);
+
+            //out js file to 8000/main.js from here
+            File mainjs = new File("src/fronted/main.js");
+            byte[] jsBytes = Files.readAllBytes(mainjs.toPath());
+            exchange.sendResponseHeaders(200, jsBytes.length);
+            exchange.getResponseHeaders().set("Content-Type", "application/javascript; charset=utf-8");
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(jsBytes);
+            }
+        }));
+
+        httpServer.createContext("/data",exchange -> {
+            String clientRequestMethod = exchange.getRequestMethod();
+            String path = exchange.getRequestURI().getPath();
+            System.out.println("request method:" + clientRequestMethod + "Path:" + path);
+
+            //get region data from csv
+            CoordinateToRegionMapper coordinateToRegionMapper = new CoordinateToRegionMapper();
+            List<String> result = coordinateToRegionMapper.getRegionList();
+
+            //jackson
+            ObjectMapper objectMapper = new ObjectMapper();
+            String JSON = objectMapper.writeValueAsString(result);
+            byte[] ByteJSON = JSON.getBytes();
+
+            exchange.sendResponseHeaders(200,ByteJSON.length);
+            exchange.getResponseHeaders().set("Content-Type","application/json");
+            try(OutputStream os = exchange.getResponseBody()){
+                os.write(ByteJSON);
+            }
+        });
 
         httpServer.start();
     }
